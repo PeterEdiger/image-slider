78e83274ee09458a93f7cb62f831a979765b9715
peet <teilzeitjobbewerber@gmx.de>
2025-07-25 21:32:12 +0200

    image-slider-component

---
+++ image-slider/README.md
@@ -0,0 +1,28 @@
+# Image Slider Component
+
+
+
+**This is a image-slider.component for reusage**
+
+**Its possible to switch gallerys/project and skip forward and back**
+
+---
+
+
+
+Disclaimer: I used my own **mixed ins in the SCSS**
+That is something you would need to refactor. 
+
+Its easy to see from the mixed in name what was used there. 
+**Just a dflex container and height width.**
+
+You would also need to define your own color because a variable is used.
+
+
+Have fun using this component. 
+
+---
+
+If you have any questions or improvements, feel free to contact me 
+
+info@peter-ediger.de
---
+++ image-slider/image-slider.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { PROJECT_ONE, PROJECT_TWO } from '../imgage_refs';
+
+@Component({
+  selector: 'app-image-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-slider.component.html',
+  styleUrl: './image-slider.component.scss'
+})
+export class ImageSliderComponent {
+
+  // === Properties ===
+  currentImgIdx = 0;
+  currentProject = PROJECT_ONE
+  currentImgSrc = this.currentProject[this.currentImgIdx];
+
+  allProjects = [
+    PROJECT_ONE,
+    PROJECT_TWO
+  ]
+
+
+  // === Methods ===
+  increaseCount() {
+    console.log("This one is firing");
+    console.log(this.currentProject[0]);
+    console.log(this.currentImgSrc);
+    this.currentImgIdx = (this.currentImgIdx +1) % this.currentProject.length;
+    this.currentImgSrc = this.currentProject[this.currentImgIdx];
+}
+
+
+  decreaseCount() {
+    this.currentImgIdx = (this.currentImgIdx -1 + this.currentProject.length) % this.currentProject.length
+    this.currentImgSrc = this.currentProject[this.currentImgIdx];
+}
+
+
+ switchCurrentProject(projectNum: number){
+  this.currentProject = this.allProjects[projectNum]
+  this.currentImgSrc = this.currentProject[0]
+ }
+
+
+}
